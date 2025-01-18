151a398536d95d64db084a7a8dcf77d205d675ff
Loew Yablonsky <tryanotherhand@gmail.com>
2025-01-18 13:26:10 +0300

    download-4

--- app/js/main.js
+++ app/js/main.js
@@ -24,3 +24,26 @@ burgerBtn.addEventListener('click', () => {
     menu.classList.toggle('active');
     stopScroll.classList.toggle('stop-scroll');
 });
+
+
+function noHover() {
+    if (window.innerWidth <= 900) {
+        document.body.classList.add('no-hover');
+      } else {
+        document.body.classList.remove('no-hover');
+      }
+}
+
+window.addEventListener('resize', noHover);
+noHover();
+
+const galleryBtn = document.querySelectorAll('.gallery__select')
+
+galleryBtn.forEach((item) => {
+    item.addEventListener('click', () => {
+        galleryBtn.forEach((item) => {
+            item.classList.remove('active');
+        })
+        item.classList.add('active');
+    })
+})
